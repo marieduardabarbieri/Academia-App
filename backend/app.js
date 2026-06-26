9665e7117c1746c8ad5884d0ca50b9a793ad2a61
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-26 10:43:49 -0400

    Alterando o app

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const { conectarBanco } = require("./config/mongo");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+async function iniciarServidor() {
+  await conectarBanco();
+
+  app.listen(3000, () => {
+    console.log("🚀 Servidor rodando na porta 3000");
+  });
+}
+
+iniciarServidor();
