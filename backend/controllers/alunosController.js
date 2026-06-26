a94fc578c1a5c1e7cdb0811926fae6491b7755bf
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-26 11:09:15 -0400

    Implementando rota para buscar por ID  e exclusão de alunos

--- backend/controllers/alunosController.js
+++ backend/controllers/alunosController.js
@@ -42,7 +42,75 @@ async function cadastrarAluno(req, res) {
   }
 }
 
+async function buscarAlunoPorId(req, res) {
+  try {
+    const db = getDatabase();
+
+    const aluno = await db.collection("alunos").findOne({
+      _id: new ObjectId(req.params.id),
+    });
+
+    if (!aluno) {
+      return res.status(404).json({
+        erro: "Aluno não encontrado.",
+      });
+    }
+
+    res.json(aluno);
+  } catch (erro) {
+    res.status(500).json({
+      erro: erro.message,
+    });
+  }
+}
+
+async function atualizarAluno(req, res) {
+  try {
+    const db = getDatabase();
+
+    const { nome, email, idade } = req.body;
+
+    const resultado = await db.collection("alunos").updateOne(
+      {
+        _id: new ObjectId(req.params.id),
+      },
+      {
+        $set: {
+          nome,
+          email,
+          idade,
+        },
+      },
+    );
+
+    res.json(resultado);
+  } catch (erro) {
+    res.status(500).json({
+      erro: erro.message,
+    });
+  }
+}
+
+async function excluirAluno(req, res) {
+  try {
+    const db = getDatabase();
+
+    const resultado = await db.collection("alunos").deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+
+    res.json(resultado);
+  } catch (erro) {
+    res.status(500).json({
+      erro: erro.message,
+    });
+  }
+}
+
 module.exports = {
   listarAlunos,
   cadastrarAluno,
+  buscarAlunoPorId,
+  atualizarAluno,
+  excluirAluno,
 };
--- backend/routes/alunos.js
+++ backend/routes/alunos.js
@@ -5,9 +5,15 @@ const router = express.Router();
 const {
   listarAlunos,
   cadastrarAluno,
+  buscarAlunoPorId,
+  atualizarAluno,
+  excluirAluno,
 } = require("../controllers/alunosController");
 
 router.get("/", listarAlunos);
 router.post("/", cadastrarAluno);
+router.get("/:id", buscarAlunoPorId);
+router.put("/:id", atualizarAluno);
+router.delete("/:id", excluirAluno);
 
 module.exports = router;
