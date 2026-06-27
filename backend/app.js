c24d65ea57c5d2d345c0930d3c3853dcdca2be94
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-26 22:24:15 -0400

    Atualizando o app.js para receber todas as rotas

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 const alunosRoutes = require("./routes/alunos");
+const exerciciosRoutes = require("./routes/exercicios");
+const instrutoresRoutes = require("./routes/instrutores");
+const matriculasRoutes = require("./routes/matriculas");
+const treinosRoutes = require("./routes/treinos");
 
 app.use("/alunos", alunosRoutes);
+app.use("/exercicios", exerciciosRoutes);
+app.use("/instrutores", instrutoresRoutes);
+app.use("/matriculas", matriculasRoutes);
+app.use("/treinos", treinosRoutes);
 
 async function iniciarServidor() {
   await conectarBanco();
