c379ee10832d33432ddbfb286cf9cf07a64d8883
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-28 15:58:24 -0400

    implementando edição e exclusão de alunos no frontend

--- frontend/js/alunos.js
+++ frontend/js/alunos.js
@@ -1,37 +1,47 @@
 const API = "http://localhost:3000/alunos";
 
+let alunoEditando = null;
+
 const listaAlunos = document.getElementById("listaAlunos");
 const form = document.querySelector("form");
 
 // Carregar alunos
 async function carregarAlunos() {
-  const resposta = await fetch(API);
-  const alunos = await resposta.json();
+    const resposta = await fetch(API);
+    const alunos = await resposta.json();
 
-  listaAlunos.innerHTML = "";
+    listaAlunos.innerHTML = "";
 
-  alunos.forEach((aluno) => {
-    listaAlunos.innerHTML += `
+    alunos.forEach((aluno) => {
+        listaAlunos.innerHTML += `
             <tr>
                 <td>${aluno.nome}</td>
                 <td>${aluno.email}</td>
                 <td>${aluno.idade}</td>
 
-                <td>
+               <td>
 
-                    <button onclick="editarAluno('${aluno._id}')">
-                        ✏️
-                    </button>
+                <button
+                    class="btnEditar"
+                    onclick="editarAluno('${aluno._id}')">
 
-                    <button onclick="excluirAluno('${aluno._id}')">
-                        🗑️
-                    </button>
+                    <i class="fa-solid fa-pen"></i>
 
-                </td>
+                </button>
+
+                <button
+                    class="btnExcluir"
+                    onclick="excluirAluno('${aluno._id}')">
+
+                    <i class="fa-solid fa-trash"></i>
+
+                </button>
+
+            </td>
 
             </tr>
         `;
-  });
+    });
 }
 
 carregarAlunos();
@@ -39,27 +49,67 @@ carregarAlunos();
 form.addEventListener("submit", cadastrarAluno);
 
 async function cadastrarAluno(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const aluno = {
-    nome: document.getElementById("nome").value,
+    const aluno = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        idade: Number(document.getElementById("idade").value),
+    };
 
-    email: document.getElementById("email").value,
+    if (alunoEditando) {
+        await fetch(`${API}/${alunoEditando}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(aluno)
 
-    idade: Number(document.getElementById("idade").value),
-  };
+        });
+        alunoEditando = null;
+        document.getElementById("btnSalvar").innerText = "Salvar";
 
-  await fetch(API, {
-    method: "POST",
+    } else {
 
-    headers: {
-      "Content-Type": "application/json",
-    },
+        await fetch(API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(aluno)
+        });
 
-    body: JSON.stringify(aluno),
-  });
+    }
 
-  form.reset();
+    form.reset();
+    carregarAlunos();
+}
 
-  carregarAlunos();
+async function editarAluno(id) {
+    const resposta = await fetch(`${API}/${id}`);
+
+    const aluno = await resposta.json();
+    alunoEditando = id;
+
+    document.getElementById("nome").value = aluno.nome;
+    document.getElementById("email").value = aluno.email;
+    document.getElementById("idade").value = aluno.idade;
+    document.getElementById("btnSalvar").innerText = "Atualizar";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+async function excluirAluno(id) {
+    const confirmar = confirm("Deseja excluir este aluno?");
+
+    if (!confirmar) return;
+    await fetch(`${API}/${id}`, {
+        method: "DELETE"
+    });
+
+    listarAlunos();
 }
